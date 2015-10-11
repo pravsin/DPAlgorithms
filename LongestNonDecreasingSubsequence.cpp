4d9aad6c535a15dc564cfafed603ecb75b388b0f
praveen <praveensingh.iitk@gmail.com>
2015-10-12 01:42:32 +0530

    Add Longest Non Decreasing Subsequence

--- LongestNonDecreasingSubsequence.cpp
+++ LongestNonDecreasingSubsequence.cpp
@@ -3,7 +3,38 @@
 using namespace std;
 
 //O(n^2) solution
-long LongestNonDecreasingSubsequence(vector<int> arr){
-
-
+long LongestNonDecreasingSubsequence1(vector<int> arr){
+    long n = arr.size();
+    vector<int> longest(n,1),pre(n);
+    for (int i = 0; i < n; ++i) {
+        pre[i]=i;
+    }
+    for (int i = 1; i < n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            if(arr[j]<=arr[i]){
+                if(longest[j]+1 > longest[i]) {
+                    longest[i] = longest[j] + 1;
+                    pre[i]=j;
+                }
+            }
+        }
+    }
+    int maxVal=0;
+    for (int k = 1; k < n; ++k) {
+        if(longest[k]>longest[maxVal])
+            maxVal = k;
+    }
+    cout<<"Length of longest Non Decreasing Subsequence is "<<longest[maxVal]<<"\n";
+    int t = maxVal, newt = maxVal;
+    stack<int> lis;
+    do {
+        t = newt;
+        lis.push(arr[t]);
+        newt = longest[t];
+    } while (t != newt);
+    while (!lis.empty()) {
+        cout << lis.top() << " ";
+        lis.pop();
+    }
+    return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,39 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+long LongestNonDecreasingSubsequence(vector<int> arr){
+    long n = arr.size();
+    vector<int> longest(n,1),pre(n);
+    for (int i = 0; i < n; ++i) {
+        pre[i]=i;
+    }
+    for (int i = 1; i < n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            if(arr[j]<=arr[i]){
+                if(longest[j]+1 > longest[i]) {
+                    longest[i] = longest[j] + 1;
+                    pre[i]=j;
+                }
+            }
+        }
+    }
+    int maxVal=0;
+    for (int k = 1; k < n; ++k) {
+        if(longest[k]>longest[maxVal])
+            maxVal = k;
+    }
+    cout<<"Length of longest Non Decreasing Subsequence is "<<longest[maxVal]<<"\n";
 
+
+    cout<<"\n";
+    for (int l = 0; l < n; ++l) {
+        cout<<pre[l]<<" ";
+    }
+
+    return 0;
+}
 int main() {
-    vector<int> arr{-7, 9, 10, 3,2, 3, 8, 8, 1};
-   // LongestIncreasingSubsequence(arr);
+    vector<int> arr{ 100, 22, 9, 33, 21, 50, 41, 60, 50,50,50 };
+    LongestNonDecreasingSubsequence(arr);
     return 0;
 }

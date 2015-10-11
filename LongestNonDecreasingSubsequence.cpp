38ff670602a15422b3a6163afa541d9316b741eb
praveen <praveensingh.iitk@gmail.com>
2015-10-12 03:29:51 +0530

    Add Longest Non Decreasing Subsequence - printing the subsequence

--- LongestNonDecreasingSubsequence.cpp
+++ LongestNonDecreasingSubsequence.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 //O(n^2) solution
-long LongestNonDecreasingSubsequence1(vector<int> arr){
+long LongestNonDecreasingSubsequence(vector<int> arr){
     long n = arr.size();
     vector<int> longest(n,1),pre(n);
     for (int i = 0; i < n; ++i) {
@@ -25,16 +25,14 @@ long LongestNonDecreasingSubsequence1(vector<int> arr){
             maxVal = k;
     }
     cout<<"Length of longest Non Decreasing Subsequence is "<<longest[maxVal]<<"\n";
-    int t = maxVal, newt = maxVal;
-    stack<int> lis;
-    do {
-        t = newt;
-        lis.push(arr[t]);
-        newt = longest[t];
-    } while (t != newt);
-    while (!lis.empty()) {
-        cout << lis.top() << " ";
-        lis.pop();
-    }
+    int j ,t= maxVal;
+    //Printing the subsequence in reverse order
+    // use vector<int> to store the result and then reverse it and then print it if want in non-decreasing order
+    do{
+        j=t;
+        cout<<arr[j]<<" ";
+        t=pre[j];
+    }while(j!=t);
+
     return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,39 +1,9 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-long LongestNonDecreasingSubsequence(vector<int> arr){
-    long n = arr.size();
-    vector<int> longest(n,1),pre(n);
-    for (int i = 0; i < n; ++i) {
-        pre[i]=i;
-    }
-    for (int i = 1; i < n; ++i) {
-        for (int j = 0; j < i; ++j) {
-            if(arr[j]<=arr[i]){
-                if(longest[j]+1 > longest[i]) {
-                    longest[i] = longest[j] + 1;
-                    pre[i]=j;
-                }
-            }
-        }
-    }
-    int maxVal=0;
-    for (int k = 1; k < n; ++k) {
-        if(longest[k]>longest[maxVal])
-            maxVal = k;
-    }
-    cout<<"Length of longest Non Decreasing Subsequence is "<<longest[maxVal]<<"\n";
 
-
-    cout<<"\n";
-    for (int l = 0; l < n; ++l) {
-        cout<<pre[l]<<" ";
-    }
-
-    return 0;
-}
 int main() {
-    vector<int> arr{ 100, 22, 9, 33, 21, 50, 41, 60, 50,50,50 };
-    LongestNonDecreasingSubsequence(arr);
+    vector<int> arr{ 8,1,6,5,4,3,2,1,9,10 };
+   // LongestNonDecreasingSubsequence(arr);
     return 0;
 }

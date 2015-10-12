82df697ae4928b31a57444ef70556df6c2637770
praveen <praveensingh.iitk@gmail.com>
2015-10-12 18:42:03 +0530

    v1.4

--- BitsMagic.cpp
+++ BitsMagic.cpp
@@ -16,3 +16,88 @@ using namespace std;
 #define turnOnLastZero(S) ((S) | (S + 1))
 #define turnOffLastConsecutiveBits(S) ((S) & (S + 1))
 #define turnOnLastConsecutiveZeroes(S) ((S) | (S - 1))
+
+
+int L[1005][1005];
+int D[1005][1005];
+
+void convert10tob(int N, int b) {
+    if (N == 0)
+        return;
+
+    int x = N % b;
+    N /= b;
+    if (x < 0)
+        N += 1;
+
+    convert10tob(N, b);
+    printf("%d", x < 0 ? x + (b * -1) : x);
+    return;
+}
+
+void multiply_by_scalar(vector<int> &a, int n) {
+    if (!n) {
+        a.clear();
+        return;
+    }
+    int carry = 0;
+    for (int i = 0; i < a.size(); ++i) {
+        a[i] = a[i] * n + carry;
+        carry = a[i] / 10;
+        a[i] %= 10;
+    }
+    for (; carry; carry /= 10)
+        a.push_back(carry % 10);
+}
+
+void print(const vector<int> &a) {
+    if (a.empty()) printf("0");
+    else {
+        for (int i = (int) a.size() - 1; i >= 0; --i)
+            printf("%d", a[i]);
+    }
+}
+
+void calfact() {
+    vector<int> a(1, 1);
+    int N =10;
+    for (int i = 2; i <= N; ++i)
+        multiply_by_scalar(a, i);
+    print(a);
+}
+
+int NLCS(string X,string Y)
+{
+    int m = X.length(),n=Y.length();
+    int i,j;
+    for (i=0;i<=m;i++)
+    {
+        for (j=0;j<=n;j++)
+        {
+            if (i==0 || j==0)
+                D[i][j]=1;
+            else
+            {
+                D[i][j]=0;
+                if (X[i-1]==Y[j-1])
+                {
+                    D[i][j]=D[i-1][j-1];
+                }
+                else
+                {
+                    if (L[i-1][j]==L[i][j])
+                        D[i][j]=(D[i][j]+D[i-1][j])%23102009;
+                    if (L[i][j-1]==L[i][j])
+                        D[i][j]=(D[i][j]+D[i][j-1])%23102009;
+                    if (L[i-1][j-1]==L[i][j])
+                    {
+                        if (D[i][j]<D[i-1][j-1])
+                            D[i][j]+=23102009;
+                        D[i][j]=(D[i][j]-D[i-1][j-1]);
+                    }
+                }
+            }
+        }
+    }
+    return (D[m][n]);
+}
---
+++ Zalgorithm.h
@@ -0,0 +1,14 @@
+//
+// Created by praveen on 12/10/15.
+//
+
+#ifndef DPALGORITHMS_ZALGORITHM_H
+#define DPALGORITHMS_ZALGORITHM_H
+
+
+class Zalgorithm {
+
+};
+
+
+#endif //DPALGORITHMS_ZALGORITHM_H
